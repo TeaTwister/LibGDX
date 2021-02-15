d1d3b001e279ee79b93bf39a930a1b8537b3f993
VS <72758495+TeaTwister@users.noreply.github.com>
2021-02-15 18:48:23 +0300

    homework 3

---
+++ core/src/ru/geekbrains/stargame/sprite/Ship.java
@@ -0,0 +1,52 @@
+package ru.geekbrains.stargame.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.math.Rect;
+
+public class Ship extends Sprite {
+    private final float CRUISING_SPEED = 0.01f;
+
+    private Vector2 spd;
+    private Vector2 movementTarget;
+    private Vector2 route = new Vector2();
+
+    private boolean moving = false;
+
+    public Ship(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        this.pos.set(worldBounds.pos);
+        setHeightProportion(0.1f);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        if (moving) move();
+        super.draw(batch);
+    }
+
+    private void move() {
+        route.set(movementTarget);
+        if (route.sub(pos).len2() <= spd.len2()) {
+            pos.add(route);
+            moving = false;
+        } else {
+            pos.add(spd);
+        }
+    }
+
+    public void setMovementTarget(Vector2 v2) {
+        movementTarget = v2;
+        spd = v2.cpy()
+                .sub(pos)
+                .setLength(CRUISING_SPEED);
+        moving = true;
+    }
+}
